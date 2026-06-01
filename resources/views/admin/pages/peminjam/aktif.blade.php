4b8dd3a72b52f4ab18ad4490b66cfca4c3217400
Vira <viraaulia244@gmail.com>
2026-06-01 15:19:55 +0700

    (Vira) dashboard admin(rata kiri header peminjaman aktif)

--- resources/views/admin/pages/peminjam/aktif.blade.php
+++ resources/views/admin/pages/peminjam/aktif.blade.php
@@ -4,8 +4,8 @@
 <div class="main-content">
 <section class="section">
 
-    <div class="section-header justify-content-center">
-        <h1>Peminjaman Aktif</h1>
+    <div class="section-header">
+        <h1>Halaman Peminjaman Aktif</h1>
     </div>
 
     <div class="section-body">
